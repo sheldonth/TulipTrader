c5b7f81f84077910da285945e112534349f4ff98
Sheldon Thomas <sheldon@resplendent.co>
2013-05-09 17:51:12 -0400

    more graphing code

---
+++ TulipTrader/TulipTrader/Categories/NSColor+Hex.h
@@ -0,0 +1,15 @@
+//
+//  NSColor+Hex.h
+//  TulipTrader
+//
+//  Created by Sheldon Thomas on 5/9/13.
+//  Copyright (c) 2013 Resplendent G.P. Sheldon Thomas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSColor (Hex)
+
++(NSColor*)colorWithHexString:(NSString*)string;
+
+@end
--- TulipTrader/TulipTrader/Views/NSView/TTGraphView.m
+++ TulipTrader/TulipTrader/Views/NSView/TTGraphView.m
@@ -10,12 +10,16 @@
 #import <CorePlot/CorePlot.h>
 #import "RUConstants.h"
 
+#define kTTGraphStrongSidePadding 55.f
+#define kTTGraphWeakSidePadding 15.f
 
 @interface TTGraphView()
 
 @property(nonatomic, retain)CPTXYGraph* graph;
 @property(nonatomic, retain)CPTGraphHostingView* defaultLayerHostingView;
 @property(nonatomic, retain)CPTMutableTextStyle* textStyle;
+
+CPTMutableTextStyle* textStyleForCurrency(TTGoxCurrency currency);
 
 @end
 
@@ -37,6 +41,26 @@ static NSString* titleFontName;
         {NSException* e = [NSException exceptionWithName:NSInternalInconsistencyException reason:@"No graph object" userInfo:nil]; @throw e;}
     [self.graph applyTheme:[CPTTheme themeNamed:kCPTStocksTheme]];
     [self.graph setTitle:stringFromCurrency(currency)];
+    [self.graph setTitleTextStyle:textStyleForCurrency(currency)];
+    [self.graph setTitlePlotAreaFrameAnchor:CPTRectAnchorTop];
+    self.graph.titleDisplacement = CGPointMake( 0.0f, 14.0f );
+    
+    CGFloat boundsPadding = round(self.bounds.size.width / (CGFloat)40.0);
+    
+    if (_graph.titleDisplacement.y > 0.0) {
+        _graph.paddingTop = _graph.titleDisplacement.y * 3;
+    }
+    else {
+        _graph.paddingTop = boundsPadding;
+    }
+    _graph.paddingLeft = boundsPadding;
+    _graph.paddingRight = boundsPadding;
+    _graph.paddingBottom = boundsPadding;
+    
+    _graph.plotAreaFrame.paddingTop    = kTTGraphWeakSidePadding;
+    _graph.plotAreaFrame.paddingRight  = kTTGraphWeakSidePadding;
+    _graph.plotAreaFrame.paddingBottom = kTTGraphStrongSidePadding;
+    _graph.plotAreaFrame.paddingLeft   = kTTGraphStrongSidePadding;
 }
 
 - (id)initWithFrame:(NSRect)frame
@@ -71,9 +95,13 @@ CPTMutableTextStyle* textStyleForCurrency(TTGoxCurrency currency)
         {
             CPTMutableTextStyle* style = [CPTMutableTextStyle textStyle];
             [style setColor:[CPTColor grayColor]];
+            [style setFontName:titleFontName];
+            [style setFontSize:24.f];
+            return style;
             break;
         }
     }
+    return nil;
 }
 
 @end
